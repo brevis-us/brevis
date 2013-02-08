9dac8422f7381488d9829961b4677d5d557bae1b
Kyle Harrington <kyleh@cs.brandeis.edu>
2013-02-08 11:11:17 -0500

    Version bump.

--- project.clj
+++ project.clj
@@ -1,8 +1,10 @@
-(defproject brevis "0.1.0-SNAPSHOT"
+(defproject brevis "0.1.1"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.4.0"]
+                 [org.clojure/math.numeric-tower "0.0.2"]
                  [kephale/cantor "0.4.1"]
                  [kephale/penumbra "0.6.1"]])
+;[org.clojars.nakkaya/ode4j "0.11.1"]
