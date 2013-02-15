982df92d1ef7964ff41fdc2bc36e0d9155c58638
Kyle Harrington <kyleh@cs.brandeis.edu>
2013-02-14 22:19:35 -0500

    Just did a fresh checkout test and found out this file was missing.

--- src/brevis/graphics/basic_3D.clj
+++ src/brevis/graphics/basic_3D.clj
@@ -2,6 +2,7 @@
   (:use [penumbra opengl compute]
         [penumbra.opengl core]
         [cantor]
+        [brevis.physics.space]
         [brevis.shape.box])
   (:require [penumbra.app :as app]
             [penumbra.text :as text]
@@ -12,16 +13,19 @@
 (defn draw-shape
   "Draw a shape. Call this after translating, scaling, and setting color."
   [obj]
-  (push-matrix
-   #_(apply color (:color obj))
-   (material :front-and-back
-    :ambient-and-diffuse (into [] (conj (:color obj) 1)); [1 1 1 1]
-    :specular            [0.5 0.4 0.4 1]
-    :shininess           64)
-   (translate (:position obj))
-   (apply scale (:dim (:shape obj)))
-   (rotate (.x (:velocity obj)) 1 0 0)
-   (rotate (.y (:velocity obj)) 0 1 0)
-   (rotate (.z (:velocity obj)) 0 0 1)
-   (draw-textured-cube)
-   #_(call-display-list (cond (= (:type (:shape obj)) :box) box-graphic))))
+  (let [pos (get-position obj)
+        vel (get-velocity obj)
+        col (:color obj)]
+	  (push-matrix
+	   #_(apply color (:color obj))
+	   (material :front-and-back
+	    :ambient-and-diffuse (into [] (conj col 1)); [1 1 1 1]
+	    :specular            [0.5 0.4 0.4 1]
+	    :shininess           64)
+	   (translate pos)
+	   (apply scale (:dim (:shape obj)))
+	   (rotate (.x vel) 1 0 0)
+	   (rotate (.y vel) 0 1 0)
+	   (rotate (.z vel) 0 0 1)
+	   (draw-textured-cube)
+	   #_(call-display-list (cond (= (:type (:shape obj)) :box) box-graphic)))))
