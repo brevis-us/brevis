0640bb95c2469129368a19dc039b5725fad9e14e
Kyle Harrington <kyleh@cs.brandeis.edu>
2013-04-19 00:01:18 -0400

    Cleanup. Floor texture.

--- src/brevis/graphics/basic_3D.clj
+++ src/brevis/graphics/basic_3D.clj
@@ -3,7 +3,7 @@
         [penumbra.opengl core]
         [cantor]
         [brevis.physics.space]
-        [brevis.shape box sphere])
+        [brevis.shape core box sphere])
   (:require [penumbra.app :as app]
             [penumbra.text :as text]
             [penumbra.data :as data]
@@ -17,21 +17,26 @@
 (defn draw-shape
   "Draw a shape. Call this after translating, scaling, and setting color."
   [obj]
-  (let [pos (get-position obj)
-        vel (get-velocity obj)
-        col (:color obj)]
-	  (push-matrix
-	   #_(apply color (:color obj))
-	   (material :front-and-back
-	    :ambient-and-diffuse (into [] (conj col 1)); [1 1 1 1]
-	    :specular            [0.5 0.4 0.4 1]
-	    :shininess           64)
-	   (translate pos)
-	   (apply scale (:dim (:shape obj)))
-	   (rotate (.x vel) 1 0 0)
-	   (rotate (.y vel) 0 1 0)
-	   (rotate (.z vel) 0 0 1)
-	   (cond
-      (= (:type (:shape obj)) :box) (draw-textured-cube)
-      (= (:type (:shape obj)) :sphere) (draw-sphere))  
-	   #_(call-display-list (cond (= (:type (:shape obj)) :box) box-graphic)))))
+  (if (:texture obj)          
+    (with-enabled :texture-2d
+      (with-texture brevis.shape.core/*checkers*;(:texture obj)      
+        (draw-shape (dissoc obj :texture))))
+	  (let [pos (get-position obj)
+	        vel (get-velocity obj)
+	        col (:color obj)]
+		  (push-matrix
+		   #_(apply color (:color obj))
+		   (material :front-and-back
+		    :ambient-and-diffuse (into [] (conj col 1)); [1 1 1 1]
+;		    :specular            [0.5 0.4 0.4 1]
+		    :shininess           80)
+		   (translate pos)
+		   (apply scale (:dim (:shape obj)))
+		   (rotate (.x vel) 1 0 0)
+		   (rotate (.y vel) 0 1 0)
+		   (rotate (.z vel) 0 0 1)
+		   (cond
+	      (= (:type (:shape obj)) :box) (draw-textured-cube)
+	      (= (:type (:shape obj)) :sphere) (draw-sphere))  
+		   #_(call-display-list (cond (= (:type (:shape obj)) :box) box-graphic))))))
+	  
