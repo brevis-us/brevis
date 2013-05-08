a96dd334c00ef34e530c26f721fc81cbaa649b1c
Kyle Harrington <kyleh@cs.brandeis.edu>
2013-05-08 12:10:27 -0400

    Version bump.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject brevis "0.2.4"
+(defproject brevis "0.2.5"
   :description "A Second-Generation Artificial Life Simulator"
   :url "https://github.com/kephale/brevis"
   :license {:name "Eclipse Public License"
