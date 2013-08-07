60d17d1986630e11635c7ae972cf7eea8aae720c
Kyle Harrington <kyleh@cs.brandeis.edu>
2013-08-07 11:17:11 -0500

    Stashing changes

--- java/brevis/Engine.java
+++ java/brevis/Engine.java
@@ -1,3 +1,20 @@
+/*
+ * brevis is free software: you can redistribute it and/or modify                                                                                                           
+    it under the terms of the GNU General Public License as published by                                                                                                             
+    the Free Software Foundation, either version 3 of the License, or                                                                                                                
+    (at your option) any later version.                                                                                                                                              
+                                                                                                                                                                                     
+    brevis is distributed in the hope that it will be useful,                                                                                                                
+    but WITHOUT ANY WARRANTY; without even the implied warranty of                                                                                                                   
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the                                                                                                                    
+    GNU General Public License for more details.                                                                                                                                     
+                                                                                                                                                                                     
+    You should have received a copy of the GNU General Public License                                                                                                                
+    along with brevis.  If not, see <http://www.gnu.org/licenses/>.                                                                                                          
+                                                                                                                                                                                     
+	Copyright 2012, 2013 Kyle Harrington
+ */
+
 package brevis;
 
 import java.util.AbstractMap.SimpleEntry;
