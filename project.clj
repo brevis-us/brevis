6f7b01c19ceb6e1e7cbaee94ae739a66887c94c5
Kyle Harrington <kyleh@cs.brandeis.edu>
2013-11-28 17:54:29 -0500

    UI deserves a version bump, but we'll delay on a push/starting to use this version # until the UI is more functional

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject brevis "0.6.1"
+(defproject brevis "0.7.0"
   :description "A Second-Generation Artificial Life Simulator"
   :url "https://github.com/kephale/brevis"
   :license {:name "Eclipse Public License"
