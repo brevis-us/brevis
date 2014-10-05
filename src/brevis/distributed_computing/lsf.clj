5ca8dcb06ee513b3888b58efdd5d8f9554814620
Kyle Harrington <kyleh@cs.brandeis.edu>
2014-10-05 14:36:03 -0400

    LSF job arrays

--- src/brevis/distributed_computing/lsf.clj
+++ src/brevis/distributed_computing/lsf.clj
@@ -14,20 +14,20 @@
     along with brevis.  If not, see <http://www.gnu.org/licenses/>.                                                                                                          
                                                                                                                                                                                      
 Copyright 2012-2014 Kyle Harrington
-
-This namespace was first contributed by Alex Bardasu.
 "
 
 (ns brevis.distributed-computing.lsf
-  (:use [clojure.java.shell])
-  (:require [me.raynes.conch :refer [programs with-programs let-programs]])
+  (:use [clojure.java.shell]
+        [clojure.math.numeric-tower])
+  (:require [me.raynes.conch :refer [programs with-programs let-programs]]
+            [clojure.string :as string])
   (:import [java.io]))
 
 ;; Function that takes a list of argmaps and generates and starts runs for the cluster
 ;; (ssh call)
 ;; Later: hook class that lets you listen to system signals -> dump to serialized files
 
-(def debug false)
+(def debug true)
 (programs ssh)
 
 #_(defn local-command
@@ -69,13 +69,11 @@ This namespace was first contributed by Alex Bardasu.
   "Takes a set of params(argmap), a configFileName, and the output
 parameters to be used in the output log, then generates the
 appropriate configuration file to be passed to the hpc."
-  [argmap configFileName namespace expName basedir profile]
+  [argmap configFileName namespace expName basedir]
   (let [out-str (str
                   "#!/bin/sh\n"
-                  "source /etc/profile;\n"
                   "cd " basedir ";\n"
-                  "lein " (if profile (str "with-profile " profile " ") "") 
-                  "run -m "
+                  "lein run -m "
                   namespace
                   #_expName
                   " "
@@ -87,20 +85,37 @@ appropriate configuration file to be passed to the hpc."
     (when debug (println "gen-config:" configFileName out-str))    
     (spit configFileName out-str)))
 
+(defn gen-command
+  "Takes a set of params(argmap), a configFileName, and the output
+parameters to be used in the output log, then generates the
+appropriate configuration file to be passed to the hpc."
+  [argmap namespace basedir profile-name]
+  (let [out-str (str
+                  "cd " basedir "; "
+                  "lein "
+                  (if profile-name (str "with-profile " profile-name " ") "")
+                  "run -m "
+                  namespace
+                  #_expName
+                  " "
+                  (serialize-map argmap " "))]
+    (when debug (println "command:"  out-str))    
+    out-str))
+
 (defn launch-config
   "Launches an experiment from the configuration file numruns times."
   [username server expName configFile numruns duration]
-  (let [command (str "source /etc/profile; bsub -W " duration " -J " expName "[1-" (str numruns) "] sh " configFile)]
+  (let [command (str "bsub -W " duration " -J " expName "[1-" (str numruns) "] " configFile)]
         #_(str "bsub " optArgs " -t 1-" (str numruns) " -N " expName " " configFile)
     (when debug (println "launch-config:" command))
     (remote-command username server command)))
 
 (defn start-runs
-  [argmaps namespace expName username server numruns source destination duration profile]
+  [argmaps namespace expName username server numruns source destination duration]
   (loop [conf 0
          argmaps argmaps]
     (when-not (empty? argmaps)
-          (gen-config (first argmaps) (str "job_" expName "_" conf ".sh") namespace expName (str destination expName) profile)
+          (gen-config (first argmaps) (str "job_" expName "_" conf ".sh") namespace expName (str destination expName))
           (recur (inc conf)
                  (rest argmaps))))
   (upload-files username server (str source "/") (str destination expName "/"))
@@ -115,3 +130,41 @@ appropriate configuration file to be passed to the hpc."
     (Thread/sleep 0.1)
     #_(Thread/sleep 0.01))
   (println "All runs submitted."))
+
+(defn launch-array
+  "Launches an experiment from the configuration file numruns times."
+  [username server expName jobFile numjobs duration]
+  (let [command (str "source /etc/profile; bsub -W " duration " -J " expName "[1-" (str numjobs) "] sh " jobFile)]
+        #_(str "bsub " optArgs " -t 1-" (str numruns) " -N " expName " " configFile)
+    (when debug (println "launch-config:" command))
+    (remote-command username server command)))
+
+(defn start-run-array
+  [argmaps namespace expName username server numruns source destination duration profile-name]
+  (let [command-list (for [run-id (range numruns)
+                           argmap argmaps]
+                       (gen-command argmap namespace (str destination expName) profile-name))
+        command-filename (str expName "_commands.sh")
+        job-filename (str expName "_job.sh")
+        max-jobs 1000]
+    ;; Write command list
+    (spit command-filename
+          (string/join "\n" command-list))
+    ;; Write command list job
+    (if (> (count command-list) max-jobs)
+      (spit job-filename
+            (str "#!/bin/bash\n"
+                 (string/join "\n"
+                              (for [rep (ceil (/ (count command-list) max-jobs))]
+                                (str "sed -n -e ''$(($LSB_JOBINDEX+" rep  "*" max-jobs "))'p' """ command-filename " | sh")))))
+      (spit job-filename
+            (str "#!/bin/bash                                                                                                                                                                                             
+sed -n -e \"$LSB_JOBINDEX p\" " command-filename " | sh")))
+    (upload-files username server (str source "/") (str destination expName "/"))
+    (println "Uploaded files.")
+    (Thread/sleep 2)
+    (println "Remotely configuring project.")
+    (remote-command username server (str "cd " destination expName "; lein clean; lein compile;"))
+    (println "Configuration complete.")
+    (launch-array username server expName (str destination expName "/" job-filename) (min max-jobs (count command-list)) duration)
+    (println "All runs submitted.")))
