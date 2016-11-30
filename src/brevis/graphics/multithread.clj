9f3390611ebb0f77b1140f391007b6b11002922e
Kyle I S Harrington <kephale@users.noreply.github.com>
2016-11-29 20:11:05 -0800

    Fixing non-graphical mode to use parameters (#14)

--- src/brevis/graphics/multithread.clj
+++ src/brevis/graphics/multithread.clj
@@ -1,4 +1,5 @@
 (ns brevis.graphics.multithread
+  (:require [brevis.parameters :as parameters])
   (:use [brevis globals])
   (:import (java.util.concurrent TimeUnit)
            (org.lwjgl.opengl SharedDrawable)
@@ -7,7 +8,8 @@
 (defn begin-with-graphics-thread
    "Everything that follows will have a GL context."
    []
-   (when (:gui @brevis.globals/*gui-state*)
+   (when (parameters/get-param :gui)
+     #_(:gui @brevis.globals/*gui-state*)
      (.tryLock ^ReentrantLock (:lock @brevis.globals/*graphics*) 250 ^TimeUnit TimeUnit/MILLISECONDS)
      (when-not (.isHeldByCurrentThread ^ReentrantLock (:lock @brevis.globals/*graphics*))
        (throw (Exception. "Can't acquire graphics lock")))
@@ -17,7 +19,7 @@
 (defn end-with-graphics-thread
   "Finish up with the graphics thread."
   []
-  (when (:gui @brevis.globals/*gui-state*)
+  (when (parameters/get-param :gui) 
     (when (.isCurrent ^SharedDrawable (:drawable @brevis.globals/*graphics*))
           (.releaseContext ^SharedDrawable (:drawable @brevis.globals/*graphics*)))
     (.unlock ^ReentrantLock (:lock @brevis.globals/*graphics*))))
