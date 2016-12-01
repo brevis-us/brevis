879303d812dceb9f9c7ee1c4723257c0a83d4611
Kyle Harrington <kharrington@uidaho.edu>
2016-11-30 18:02:48 -0800

    Updates for rocks and debugging of distributed computing

--- src/brevis/distributed_computing/dc_utils.clj
+++ src/brevis/distributed_computing/dc_utils.clj
@@ -5,7 +5,7 @@
             [clojure.string :as string])
   (:import [java.io]))
 
-(def debug false)
+(def debug-mode (atom false))
 (programs ssh)
 
 #_(defn local-command
@@ -16,7 +16,7 @@
 
 (defn local-command
    [command]
-   (when debug (println "local-command:" command))
+   (when @debug-mode (println "local-command:" command))
    #_(sh command)
    (.exec (. Runtime getRuntime) command)
    #_(let [proc (.exec (. Runtime getRuntime) command)]
@@ -27,7 +27,7 @@
   [username server command]
   (let [comm #_(str "ssh " username "@" server " \\\"" command "\\\"")
         (str "ssh " username "@" server " \"" command "\"")]
-    (when debug (println "remote-command:" comm))
+    (when @debug-mode (println "remote-command:" comm))
     (ssh (str username "@" server) command)
     #_(local-command comm)))
 
@@ -36,7 +36,7 @@
   ;; source: ../../../
   ;; destination: ~/
   (let [command (str "rsync -avzr " source " " username "@" server ":" destination)]
-    (when debug (println "upload-files:" command))    
+    (when @debug-mode (println "upload-files:" command))    
     (local-command command)))
 
 (defn serialize-map 
