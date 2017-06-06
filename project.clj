41ac472543fa860cf038ad8086ed216a046cc47f
Kyle Harrington <kharrington@uidaho.edu>
2017-06-06 13:10:23 +0200

    Slurm working on 2 different slurm clusters

--- project.clj
+++ project.clj
@@ -22,7 +22,7 @@
                  ;[potemkin "0.4.1"]
                  
                  ;; Images and Physics packages      
-                 [funimage/funimage "0.1.92-SNAPSHOT"]
+                 [funimage/funimage "0.1.97"]
                  [org.ode4j/core "0.2.8"]; 0.3.0 introduced threading issues that are causing problems
                  [org.ode4j/demo "0.2.8"]; 0.2.9 forces java7
                  [com.nitayjoffe.thirdparty.net.robowiki.knn/knn-benchmark "0.1"]; funimage can handle this, factor this out          
