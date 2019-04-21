a652a158939d3c7a2d6f6ccc4d2078bed3cd2fd2
Kyle IS Harrington <kyle.i.s.harrington@gmail.com>
2019-04-21 08:48:28 -0400

    Fix references in us.brevis.plot

--- src/main/clojure/us/brevis/plot.clj
+++ src/main/clojure/us/brevis/plot.clj
@@ -8,7 +8,7 @@
            (org.jfree.ui ApplicationFrame RefineryUtilities)
            (java.text SimpleDateFormat)
            (java.awt Color)
-           (brevis.plot Plotter))
+           (us.brevis.plot Plotter))
   (:use [us.brevis.physics.utils]; for add-update-handler
         [us.brevis utils]
         [us.brevis.random]))
@@ -20,12 +20,12 @@
   ([data] (make-xy-dataset data (gensym "dataset")))
   ([data dataset-name] (make-xy-dataset data dataset-name false false))
   ([data dataset-name auto-sort allow-duplicates]
-    (let [^XYSeries xyseries (XYSeries. dataset-name auto-sort allow-duplicates)]
-      (doseq [[k v] data]
-        (.addOrUpdate xyseries k v))
-      (let [xycoll (XYSeriesCollection. xyseries)]
-        {:data-collection xycoll
-         :series [xyseries]}))))
+   (let [^XYSeries xyseries (XYSeries. dataset-name auto-sort allow-duplicates)]
+     (doseq [[k v] data]
+       (.addOrUpdate xyseries k v))
+     (let [xycoll (XYSeriesCollection. xyseries)]
+       {:data-collection xycoll
+        :series [xyseries]}))))
 
 (defn add-plot-handler
   "Add a plot handler. Remove assumes you want to remove the min x, value, this is best for timeseries"
@@ -37,7 +37,7 @@
                  priority 100}}]
   (when-not (System/getProperty "brevisHeadless")
     (let [plot-data (make-xy-dataset [] title)
-          plotter (brevis.plot.Plotter. title (:data-collection plot-data))
+          plotter (Plotter. title (:data-collection plot-data))
           handler-fn (fn []
                        (when (> (.getItemCount (first (:series plot-data))) interval)
                          (.remove ^XYSeries (first (:series plot-data)) 
@@ -65,7 +65,7 @@
                  priority 100}}]
   (when-not (System/getProperty "brevisHeadless")
     (let [plot-data (make-xy-dataset [] title)
-          plotter (brevis.plot.Plotter. title (:data-collection plot-data))
+          plotter (Plotter. title (:data-collection plot-data))
           handler-fn (fn []
                        (.clear ^XYSeries (first (:series plot-data)))
                        (doseq [[x y] (xy-fn)]
@@ -86,9 +86,9 @@
    "Convert a vector of elements into a plottable histogram dataset."
    ([data] (make-histogram-dataset data (gensym "dataset")))
    ([data dataset-name]
-     (let [dataset (SimpleHistogramDataset. dataset-name (into (double-array)
-                                                               data))]
-       {:series [dataset]})))
+    (let [dataset (SimpleHistogramDataset. dataset-name (into (double-array)
+                                                              data))]
+      {:series [dataset]})))
 
 (defn add-histogram-handler
   "Add a plot handler. Plots a lazy histogram, requires numbers."
@@ -98,7 +98,7 @@
                 priority 100}}]
   (when-not (System/getProperty "brevisHeadless")
     (let [plot-data (make-xy-dataset [] title)
-          plotter (brevis.plot.Plotter. title (:data-collection plot-data))
+          plotter (Plotter. title (:data-collection plot-data))
           handler-fn (fn []                       
                        (let [xs (x-fn)
                              hist (frequencies xs)]
@@ -119,12 +119,12 @@
   ([n] (make-multiseries-xy-dataset n (repeatedly n #(gensym "dataset"))))
   ([n dataset-names] (make-multiseries-xy-dataset n dataset-names false false))
   ([n dataset-names auto-sort allow-duplicates]
-    (let [xyseries (map #(XYSeries. % auto-sort allow-duplicates) dataset-names)]
-      (let [xycoll (XYSeriesCollection.)]
-        (doseq [s xyseries]
-          (.addSeries xycoll ^XYSeries s)) 
-        {:data-collection xycoll
-         :series (into [] xyseries)}))))
+   (let [xyseries (map #(XYSeries. % auto-sort allow-duplicates) dataset-names)]
+     (let [xycoll (XYSeriesCollection.)]
+       (doseq [s xyseries]
+         (.addSeries xycoll ^XYSeries s))
+       {:data-collection xycoll
+        :series (into [] xyseries)}))))
 
 (defn add-multiplot-handler
   "Add a plot handler. Remove assumes you want to remove the min x, value, this is best for timeseries"
@@ -142,7 +142,7 @@
     (let [n (count xy-fns)
           legends (if (empty? legends) (for [k (range n)] (str title " " k)) legends)
           plot-data (make-multiseries-xy-dataset n legends)
-          plotter (brevis.plot.Plotter. title (:data-collection plot-data))
+          plotter (Plotter. title (:data-collection plot-data))
           handler-fn (fn []
                        (dotimes [k (min (count xy-fns) interval)]
                          (let [series (nth (:series plot-data) k)
